82a76987bc47c1723b99ac0f9580db6cf7446eb4
SantaGAv32 <122312824+SantaGAv32@users.noreply.github.com>
2024-04-03 18:30:59 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <iomanip>
+int main() {
+    int n;
+    std::cout << "Введите высоту треугольника: ";
+    std::cin >> n;
+    if (n < 1) {
+        std::cerr << "Ошибка! Введите высоту треугольника" << std::endl;
+        return 1;
+    }
+
+    for (int i = 1; i <= n; ++i) {
+        int spaces = n - i;
+        int stars = 2 * i - 1;
+
+        std::cout << std::setw(spaces) << "" << std::setw(stars) << "*";
+        if (i != n) {
+            std::cout << std::endl;
+        }
+    }
+
+    return 0;
+}
